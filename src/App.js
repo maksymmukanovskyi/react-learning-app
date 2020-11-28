c53a9613e4d8f53e521235d9600c7f59297ad16a
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2020-11-28 17:23:58 +0200

    clean up for second tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Product from "./Product"
-import Specifications from "./Specifications"
 const App = () => (
   <Product 
   imgUrl="http://placeimg.com/640/480/any"
@@ -7,13 +6,6 @@ const App = () => (
   price="150$"
   name="Atlant"
   text="hey check this out man">
-
-<Specifications
-  name="architecture"
-  discount="50%"
-  unreadMessages="Tanya"
-  isLoggedIn= {false}
-/>
   </Product>
 );
 
